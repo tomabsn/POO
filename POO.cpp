3e2a08cff7c7c15ee865ac0c59f73df3f5cf5353
Thomas Besson <thomas.besson28@gmail.com>
2020-10-02 12:13:45 +0200

    exemple méthode virtual

--- POO.cpp
+++ POO.cpp
@@ -1,13 +1,32 @@
-#include <random>
 #include <iostream>
-using namespace std;
+struct Base {
+    virtual void f() {
+        std::cout << "base\n";
+    }
+};
+struct Derived : Base {
+    void f() override { // 'override' is optional
+        std::cout << "derived\n";
+    }
+};
+int main()
+{
+    Base b;
+    Derived d;
 
+    // virtual function call through reference
+    Base& br = b; // the type of br is Base&
+    Base& dr = d; // the type of dr is Base& as well
+    br.f(); // prints "base"
+    dr.f(); // prints "derived"
 
-int main() {
-    random_device rd;
-    mt19937 prng{ rd() };
-    uniform_int_distribution<int> d6{ 1, 6 };
-    for (short i = 0; i < 10; ++i)
-        cout << d6(prng) << " ";
-    cout << endl;
+    // virtual function call through pointer
+    Base* bp = &b; // the type of bp is Base*
+    Base* dp = &d; // the type of dp is Base* as well
+    bp->f(); // prints "base"
+    dp->f(); // prints "derived"
+
+    // non-virtual function call
+    br.Base::f(); // prints "base"
+    dr.Base::f(); // prints "base"
 }
